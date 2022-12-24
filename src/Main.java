3969ec6ca190f3ab1b589df9d6e2a43953a19e07
jLawyer85 <sven.weichel@icloud.com>
2022-12-24 23:17:03 +0100

    Klasse Tier lernt die Methoden bellen.

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        int z = 3;
-        for (int i=1; i<=300; i++){
-            z = (z +i) + 5;
-        }
-        System.out.println(z);
-        System.out.println("END.");
-        System.out.println(("PI mal Daumen"));
+        Tier Hund = new Tier("Hund","Bello");
+        Hund.bellen();
     }
 }
 
--- src/Tier.java
+++ src/Tier.java
@@ -1,7 +1,26 @@
 public class Tier {
     String tierart;
     String tiername;
+    public Tier(String tierart, String tiername) {
+        this.tierart = tierart;
+        this.tiername = tiername;
+    }
+    public void setTierart(String tierart) {
+        this.tierart = tierart;
+    }
+    public void setTiername(String tiername) {
+        this.tiername = tiername;
+    }
 
+    public String getTierart() {
+        return tierart;
+    }
 
+    public String getTiername() {
+        return tiername;
+    }
 
+    public void bellen(){
+        System.out.println(getTiername() + ": WuffWuff");
+    }
 }
