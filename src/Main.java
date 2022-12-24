25e4d34482b11fb0faa9fa00eafd98281ea05883
jLawyer85 <sven.weichel@icloud.com>
2022-12-24 22:24:13 +0100

    Test with Windows IntelliJ

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int z = 3;
+        for (int i=1; i<=300; i++){
+            z = (z +i) + 5;
+        }
+        System.out.println(z);
+        System.out.println("END.");
+    }
+}
+
